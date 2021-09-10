d465374b8520e08c469e56888ed07cd85c99ae57
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2021-09-10 11:48:31 +0200

    Filter for manageable groups only
    Changed to filter for Microsoft 365 and Cloud-Only Security groups, since these are only manageable by the Microsoft Graph API

--- permissions.ps1
+++ permissions.ps1
@@ -28,27 +28,61 @@ try{
         Authorization = "Bearer $accesstoken";
         'Content-Type' = "application/json";
         Accept = "application/json";
+        # Needed to filter on specific attributes (https://docs.microsoft.com/en-us/graph/aad-advanced-queries)
         ConsistencyLevel = "eventual";
     }
 
-    Write-Verbose -Verbose "Searching for AzureAD groups.."
+    # Define the properties to select (comma seperated)
+    # Add optinal popertySelection (mandatory: id,displayName,onPremisesSyncEnabled)
+    # Comment out $properties to select all properties
+    $properties = @("id", "displayName", "onPremisesSyncEnabled")
+    if ($null -ne $properties) {
+        $select = "&`$select=$($properties -join ",")"
+    }
+    else {
+        $select = $null
+    }
 
+    # Currently only Microsoft 365 and Security groups are supported by the Microsoft Graph API
+    # https://docs.microsoft.com/en-us/graph/api/resources/groups-overview?view=graph-rest-1.0
+    [System.Collections.ArrayList]$groups = @()
+
+    # Get Microsoft 365 Groups only (https://docs.microsoft.com/en-us/graph/api/group-list?view=graph-rest-1.0&tabs=http)
+    Write-Verbose -Verbose "Searching for Microsoft 365 groups.."
     $baseSearchUri = "https://graph.microsoft.com/"
-    $searchUri = $baseSearchUri + 'v1.0/groups' + '?$filter=' + "onPremisesSyncEnabled eq null" + '%26$count=true'
-    $searchUri = [System.Web.HttpUtility]::UrlDecode($searchUri)
+    $searchUri = $baseSearchUri + "v1.0/groups?`$filter=groupTypes/any(c:c+eq+'Unified')$select" 
 
     $response = Invoke-RestMethod -Uri $searchUri -Method Get -Headers $authorization -Verbose:$false
-    [System.Collections.ArrayList]$groups = $response.value
+    [System.Collections.ArrayList]$microsoft365Groups = $response.value
     while (![string]::IsNullOrEmpty($response.'@odata.nextLink')) {
         $response = Invoke-RestMethod -Uri $response.'@odata.nextLink' -Method Get -Headers $authorization -Verbose:$false
-        foreach($item in $response.value){
-            $null = $groups.Add($item)
-        }
-    }    
-    Write-Verbose -Verbose "Finished searching for AzureAD Groups. Found [$($groups.id.Count) groups]"    
+        foreach($item in $response.value){ $null = $microsoft365Groups.Add($item) }
+    }
+    Write-Verbose -Verbose "Finished searching for Microsoft 365 groups. Found [$($microsoft365Groups.id.Count) groups]"
+    foreach($microsoft365Group in $microsoft365Groups){ $null = $groups.Add($microsoft365Group) }
+
+
+    # Get Security Groups only (https://docs.microsoft.com/en-us/graph/api/resources/groups-overview?view=graph-rest-1.0)
+    Write-Verbose -Verbose "Searching for Security groups.."
+    $baseSearchUri = "https://graph.microsoft.com/"
+    # Filter Cloud-Only groups (onPremisesSyncEnabled = null) and Security Groups only (https://docs.microsoft.com/en-us/graph/api/resources/groups-overview?view=graph-rest-1.0)
+    $searchUri = $baseSearchUri + "v1.0/groups?`$filter=onPremisesSyncEnabled eq null and mailEnabled eq false and securityEnabled eq true$select"
+    # Needed to filter on specific attributes (https://docs.microsoft.com/en-us/graph/aad-advanced-queries)
+    $searchUri = $searchUri + '&$count=true'
+
+    $response = Invoke-RestMethod -Uri $searchUri -Method Get -Headers $authorization -Verbose:$false
+    [System.Collections.ArrayList]$securityGroups = $response.value
+    while (![string]::IsNullOrEmpty($response.'@odata.nextLink')) {
+        $response = Invoke-RestMethod -Uri $response.'@odata.nextLink' -Method Get -Headers $authorization -Verbose:$false
+        foreach($item in $response.value){ $null = $securityGroups.Add($item) }
+    }
+    Write-Verbose -Verbose "Finished searching for Security Groups. Found [$($securityGroups.id.Count) groups]"
+    foreach($securityGroup in $securityGroups){ $null = $groups.Add($securityGroup) }
+
 }catch{
     throw "Could not gather Azure AD groups. Error: $_"
 }
+
 
 $permissions = @(foreach($group in $groups){
     @{
